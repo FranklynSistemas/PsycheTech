0a26184b6567cca3461d4081a0a0f5032b93caca
franklynsistemas <franking.sistemas@gmail.com>
2018-01-27 17:32:20 -0500

    second changes

--- web/server.js
+++ web/server.js
@@ -10,7 +10,7 @@ require('./models/pages');
 var routes = require('./routes/routes');
 
 // Conexión con la base de datos
-mongoose.connect('mongodb:///psychetech',
+mongoose.connect('mongodb://104.197.252.243:80/psychetech',
   function(err, res) {
     if(err) throw err;
     console.log('Conectado con exito a la BD');
@@ -19,14 +19,9 @@ mongoose.connect('mongodb:///psychetech',
 // Configuración
 
 // Localización de los ficheros estaticos
-app.use(express.static(__dirname + '/public'));
-// Muestra un log de todos los request en la consola		
-//app.use(express.logger('dev'));
-// Permite cambiar el HTML con el método POST					
+app.use(express.static(__dirname + '/public'));				
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-// Simula DELETE y PUT						
-//app.use(methodOverride());
 
 
 // Escucha en el puerto 8080 y corre el server
@@ -34,8 +29,4 @@ app.listen(8080, function() {
 	console.log('App listening on port 8080');
 });
 
-/*app.get('*', function(req, res) {						
-	res.sendfile('../public/index.html');				
-});
-*/
 app.use('/', routes);
