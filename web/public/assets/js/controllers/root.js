127c43232cf8467c62cf586c8b441cc706f204e6
franklynsistemas <franking.sistemas@gmail.com>
2018-02-09 21:40:48 -0500

    add login with facebook

--- web/public/assets/js/controllers/root.js
+++ web/public/assets/js/controllers/root.js
@@ -1,6 +1,6 @@
-app.controller('rootController', function($scope, $http) {
+app.controller('rootController', function($scope, $rootScope, $http) {
   $scope.form = {}
-
+  $rootScope.isAuth = true;
   $scope.sendFrom = function(form) {
 
     const url = '/createContact';
@@ -30,5 +30,28 @@ app.controller('rootController', function($scope, $http) {
     }
 
   }
+
+  var $window = $(window);
+
+  $window.on('load', function() {
+    FB.getLoginStatus(function(response) {
+      $rootScope.isAuth =  true;
+      statusChangeCallback(response);
+    });
+
+    function statusChangeCallback(response) {
+      console.log(response);
+    }
+
+    var finished_rendering = function() {
+      console.log("finished rendering plugins");
+      var spinner = document.getElementById("spinner");
+      spinner.removeAttribute("style");
+      spinner.removeChild(spinner.childNodes[0]);
+    };
+
+    FB.Event.subscribe('xfbml.render', finished_rendering);
+  });
+
 
 });
